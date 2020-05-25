0565089bd9bd8ea1137ea36678b47f4324821e19
datbeohbbh <phongtomfapp@gmail.com>
2020-05-25 15:06:23 +0300

    file tree

---
+++ file_tree/src/main/java/com/efimchick/ifmo/io/filetree/Main.java
@@ -0,0 +1,69 @@
+package com.efimchick.ifmo.io.filetree;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        InputStream  inputstream;
+        OutputStream outputstream;
+        inputstream = System.in;
+        outputstream = System.out;
+        InputReader in = new InputReader(inputstream);
+        PrintWriter out = new PrintWriter(outputstream);
+        Task solver = new Task();
+        solver.solve(in,out);
+        out.close();
+    }
+
+    static class Task{
+        public void solve(InputReader in,PrintWriter out){
+            FileTreeImpl t = new FileTreeImpl();
+            System.out.print(t.tree(Paths.get("/home/datbeohbbh/Desktop/MY_PROJECT")));
+        }
+    }
+
+    static class InputReader{
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream){
+            reader = new BufferedReader(new InputStreamReader(stream));
+            tokenizer = null;
+        }
+
+        public String next(){
+            while(tokenizer == null || !tokenizer.hasMoreTokens()){
+                try{
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e){
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong(){
+            return Long.parseLong(next());
+        }
+
+        public  double nextDouble(){
+            return Double.parseDouble(next());
+        }
+
+        public String nextLine(){
+            String ret_str = "";
+            try {
+                ret_str = reader.readLine();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return ret_str;
+        }
+    }
+}
